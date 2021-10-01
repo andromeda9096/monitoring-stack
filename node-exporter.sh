2d5f662f6068f42bd14dec89f4990ff7b3a5529a
trungnguyen <andromeda9096@gmail.com>
2021-10-01 11:17:49 +0700

    Update node-exporter.sh

--- node-exporter.sh
+++ node-exporter.sh
@@ -23,7 +23,7 @@ echo "download and install node-exporter"
 echo ""
 echo ""
 sleep 2s
-curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest| grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
+sudo curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest| grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
 
 tar -xvf node_exporter*.tar.gz
 cd  node_exporter*/
